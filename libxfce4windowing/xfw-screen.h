4382149fa1e7f876e7696b933c0ac0a505767d59
Gaël Bonithon <gael@xfce.org>
2022-10-05 09:08:02 +0200

    Remove public multi-screen API
    Multi-screen support has been dropped since GTK 3.10.

--- libxfce4windowing/xfw-screen.h
+++ libxfce4windowing/xfw-screen.h
@@ -52,7 +52,6 @@ struct _XfwScreenIface {
     void (*window_closed)(XfwScreen *screen, XfwWindow *window);
 
     /* Virtual Table */
-    gint (*get_number)(XfwScreen *screen);
     XfwWorkspaceManager *(*get_workspace_manager)(XfwScreen *screen);
     GList *(*get_windows)(XfwScreen *screen);
     GList *(*get_windows_stacked)(XfwScreen *screen);
@@ -65,9 +64,7 @@ struct _XfwScreenIface {
 GType xfw_screen_get_type(void) G_GNUC_CONST;
 
 XfwScreen * xfw_screen_get_default(void);
-XfwScreen * xfw_screen_get(GdkScreen *gdk_screen);
 
-gint xfw_screen_get_number(XfwScreen *screen);
 XfwWorkspaceManager *xfw_screen_get_workspace_manager(XfwScreen *screen);
 GList *xfw_screen_get_windows(XfwScreen *screen);
 GList *xfw_screen_get_windows_stacked(XfwScreen *screen);
