36eddc5cf79b837abb759bd0ed52829797b4dc50
Brian J. Tarricone <brian@tarricone.org>
2023-01-19 01:15:59 -0800

    Make XfwWindow use XfwMonitor

--- libxfce4windowing/xfw-screen.c
+++ libxfce4windowing/xfw-screen.c
@@ -586,3 +586,37 @@ xfw_screen_get_monitors(XfwScreen *screen) {
 
     return priv->monitors;
 }
+
+static XfwMonitor *
+find_monitor(GList *monitors, GdkMonitor *monitor) {
+    for (GList *l = monitors; l != NULL; l = l->next) {
+        XfwMonitor *xmonitor = XFW_MONITOR(l->data);
+
+        if (xfw_monitor_get_gdk_monitor(xmonitor) == monitor) {
+            return xmonitor;
+        }
+    }
+
+    return NULL;
+}
+
+XfwMonitor *
+_xfw_screen_get_monitor_for_gdk_monitor(XfwScreen *screen, GdkMonitor *monitor) {
+    XfwMonitor *xmonitor = find_monitor(xfw_screen_get_monitors(screen), monitor);
+
+    if (G_UNLIKELY(xmonitor == NULL)) {
+        // This shouldn't happen, but I think sometimes
+        // GdkScreen::monitors-changed fails to fire in time.
+        XfwScreenPrivate *priv = XFW_SCREEN_GET_PRIVATE(screen);
+
+        g_list_free(priv->monitors);
+        priv->monitors = NULL;
+        xmonitor = find_monitor(xfw_screen_get_monitors(screen), monitor);
+    }
+
+    if (G_UNLIKELY(xmonitor == NULL)) {
+        g_critical("Unable to match GdkMonitor with XfwMonitor");
+    }
+
+    return xmonitor;
+}
