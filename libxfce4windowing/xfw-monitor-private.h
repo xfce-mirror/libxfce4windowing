398ad4d2d90603fb25aa1e8f20bd0e92eccd158c
Brian J. Tarricone <brian@tarricone.org>
2023-01-19 00:20:51 -0800

    Add initial XfwMonitor abstraction
    So far it mostly just wraps GdkMonitor, but there will be more to come.

--- libxfce4windowing/xfw-monitor-private.h
+++ libxfce4windowing/xfw-monitor-private.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Brian Tarricone <brian@tarricone.org>
+ * Copyright (c) 2023 Brian Tarricone <brian@tarricone.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,20 +17,30 @@
  * MA 02110-1301 USA
  */
 
-#ifndef __LIBXFCE4WINDOWING_H__
-#define __LIBXFCE4WINDOWING_H__
+#ifndef __XFW_MONITOR_PRIVATE_H__
+#define __XFW_MONITOR_PRIVATE_H__
 
-#define __LIBXFCE4WINDOWING_H_INSIDE__
+#if !defined(__LIBXFCE4WINDOWING_H_INSIDE__) && !defined(LIBXFCE4WINDOWING_COMPILATION)
+#error "Only libxfce4windowing.h can be included directly"
+#endif
 
-#include <libxfce4windowing/libxfce4windowing-config.h>
-#include <libxfce4windowing/xfw-monitor.h>
-#include <libxfce4windowing/xfw-screen.h>
-#include <libxfce4windowing/xfw-util.h>
-#include <libxfce4windowing/xfw-window.h>
-#include <libxfce4windowing/xfw-workspace-group.h>
-#include <libxfce4windowing/xfw-workspace-manager.h>
-#include <libxfce4windowing/xfw-workspace.h>
+#include <gdk/gdk.h>
 
-#undef __LIBXFCE4WINDOWING_H_INSIDE__
+#include "xfw-monitor.h"
 
-#endif  /* !__LIBXFCE4WINDOWING_H__ */
+G_BEGIN_DECLS
+
+struct _XfwMonitorClass {
+    /*< private >*/
+    GObjectClass parent_class;
+
+    /*< public >*/
+
+    /* Signals */
+
+    /* Virtual Table */
+};
+
+G_END_DECLS
+
+#endif  /* __XFW_MONITOR_PRIVATE_H__ */
