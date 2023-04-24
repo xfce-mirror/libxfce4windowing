xfce-mirror/libxfce4windowing

commit 9364b05146448c89224376c87f82686da4e93a66

XfwApplication: Keep a ref on XfwWindows

libxfce4windowing/xfw-application-wayland.c
@@ -176,7 +176,7 @@ xfw_application_wayland_finalize(GObject *obj) {
     for (GList *lp = priv->windows; lp != NULL; lp = lp->next) {
         g_signal_handlers_disconnect_by_data(lp->data, obj);
     }
-    g_list_free(priv->windows);
+    g_list_free_full(priv->windows, g_object_unref);
     g_list_free(priv->instances);
 
     G_OBJECT_CLASS(xfw_application_wayland_parent_class)->finalize(obj);
@@ -246,6 +246,7 @@ static void
 window_closed(XfwWindowWayland *window, XfwApplicationWayland *app) {
     g_signal_handlers_disconnect_by_data(window, app);
     app->priv->windows = g_list_remove(app->priv->windows, window);
+    g_object_unref(window);
     g_object_notify(G_OBJECT(app), "windows");
 }
 
@@ -254,6 +255,7 @@ window_application_changed(XfwWindowWayland *window, GParamSpec *pspec, XfwAppli
     if (XFW_APPLICATION(app) != xfw_window_get_application(XFW_WINDOW(window))) {
         g_signal_handlers_disconnect_by_data(window, app);
         app->priv->windows = g_list_remove(app->priv->windows, window);
+        g_object_unref(window);
         g_object_notify(G_OBJECT(app), "windows");
     }
 }
@@ -276,7 +278,7 @@ _xfw_application_wayland_get(XfwWindowWayland *window, const gchar *app_id) {
         g_object_ref(app);
     }
 
-    app->priv->windows = g_list_prepend(app->priv->windows, window);
+    app->priv->windows = g_list_prepend(app->priv->windows, g_object_ref(window));
     g_signal_connect(window, "closed", G_CALLBACK(window_closed), app);
     g_signal_connect(window, "notify::application", G_CALLBACK(window_application_changed), app);
     g_object_notify(G_OBJECT(app), "windows");

libxfce4windowing/xfw-application-x11.c
@@ -164,7 +164,7 @@ xfw_application_x11_finalize(GObject *obj) {
     for (GList *lp = priv->windows; lp != NULL; lp = lp->next) {
         g_signal_handlers_disconnect_by_data(lp->data, obj);
     }
-    g_list_free(priv->windows);
+    g_list_free_full(priv->windows, g_object_unref);
     g_hash_table_destroy(priv->instances);
     g_list_free(priv->instance_list);
 
@@ -271,6 +271,7 @@ window_closed(XfwWindowX11 *window, XfwApplicationX11 *app) {
     g_signal_handlers_disconnect_by_data(window, app);
 
     app->priv->windows = g_list_remove(app->priv->windows, window);
+    g_object_unref(window);
     g_object_notify(G_OBJECT(app), "windows");
 
     instance->windows = g_list_remove(instance->windows, window);
@@ -301,7 +302,7 @@ _xfw_application_x11_get(WnckClassGroup *wnck_group, XfwWindowX11 *window) {
         g_object_ref(app);
     }
 
-    app->priv->windows = g_list_prepend(app->priv->windows, window);
+    app->priv->windows = g_list_prepend(app->priv->windows, g_object_ref(window));
     g_signal_connect(window, "closed", G_CALLBACK(window_closed), app);
     g_object_notify(G_OBJECT(app), "windows");
 

commit 6486f0096e2d672d2318e084075b69a9efbc07c1

XfwApplication: Fix window memory management
This avoids reference cycle when the XfwScreen is released and windows
remain open. This is rarely a problem in practice but can cause crashes
that are difficult to debug.

Fixes: 9364b05146448c89224376c87f82686da4e93a66

libxfce4windowing/xfw-application-wayland.c
@@ -159,7 +159,7 @@ xfw_application_wayland_finalize(GObject *obj) {
     for (GList *lp = priv->windows; lp != NULL; lp = lp->next) {
         g_signal_handlers_disconnect_by_data(lp->data, obj);
     }
-    g_list_free_full(priv->windows, g_object_unref);
+    g_list_free(priv->windows);
     g_list_free(priv->instances);
 
     G_OBJECT_CLASS(xfw_application_wayland_parent_class)->finalize(obj);
@@ -206,7 +206,6 @@ static void
 window_closed(XfwWindowWayland *window, XfwApplicationWayland *app) {
     g_signal_handlers_disconnect_by_data(window, app);
     app->priv->windows = g_list_remove(app->priv->windows, window);
-    g_object_unref(window);
     g_object_notify(G_OBJECT(app), "windows");
 }
 
@@ -215,9 +214,13 @@ window_application_changed(XfwWindowWayland *window, GParamSpec *pspec, XfwAppli
     if (XFW_APPLICATION(app) != xfw_window_get_application(XFW_WINDOW(window))) {
         g_signal_handlers_disconnect_by_data(window, app);
         app->priv->windows = g_list_remove(app->priv->windows, window);
-        g_object_unref(window);
         g_object_notify(G_OBJECT(app), "windows");
     }
+}
+
+static void
+toggle_notify(gpointer data, GObject *object, gboolean is_last_ref) {
+  g_object_remove_toggle_ref(object, toggle_notify, data);
 }
 
 XfwApplicationWayland *
@@ -238,7 +241,10 @@ _xfw_application_wayland_get(XfwWindowWayland *window, const gchar *app_id) {
         g_object_ref(app);
     }
 
-    app->priv->windows = g_list_prepend(app->priv->windows, g_object_ref(window));
+    // avoid reference cycle
+    g_object_add_toggle_ref(G_OBJECT(window), toggle_notify, app);
+
+    app->priv->windows = g_list_prepend(app->priv->windows, window);
     g_signal_connect(window, "closed", G_CALLBACK(window_closed), app);
     g_signal_connect(window, "notify::application", G_CALLBACK(window_application_changed), app);
     g_object_notify(G_OBJECT(app), "windows");

libxfce4windowing/xfw-application-x11.c
@@ -148,7 +148,7 @@ xfw_application_x11_finalize(GObject *obj) {
     for (GList *lp = priv->windows; lp != NULL; lp = lp->next) {
         g_signal_handlers_disconnect_by_data(lp->data, obj);
     }
-    g_list_free_full(priv->windows, g_object_unref);
+    g_list_free(priv->windows);
     g_hash_table_destroy(priv->instances);
     g_list_free(priv->instance_list);
 
@@ -231,7 +231,6 @@ window_closed(XfwWindowX11 *window, XfwApplicationX11 *app) {
     g_signal_handlers_disconnect_by_data(window, app);
 
     app->priv->windows = g_list_remove(app->priv->windows, window);
-    g_object_unref(window);
     g_object_notify(G_OBJECT(app), "windows");
 
     instance->windows = g_list_remove(instance->windows, window);
@@ -240,6 +239,11 @@ window_closed(XfwWindowX11 *window, XfwApplicationX11 *app) {
         app->priv->instance_list = g_list_remove(app->priv->instance_list, instance);
         g_object_notify(G_OBJECT(app), "instances");
     }
+}
+
+static void
+toggle_notify(gpointer data, GObject *object, gboolean is_last_ref) {
+  g_object_remove_toggle_ref(object, toggle_notify, data);
 }
 
 XfwApplicationX11 *
@@ -262,7 +266,10 @@ _xfw_application_x11_get(WnckClassGroup *wnck_group, XfwWindowX11 *window) {
         g_object_ref(app);
     }
 
-    app->priv->windows = g_list_prepend(app->priv->windows, g_object_ref(window));
+    // avoid reference cycle
+    g_object_add_toggle_ref(G_OBJECT(window), toggle_notify, app);
+
+    app->priv->windows = g_list_prepend(app->priv->windows, window);
     g_signal_connect(window, "closed", G_CALLBACK(window_closed), app);
     g_object_notify(G_OBJECT(app), "windows");
 