54cea3bbc436cb19a84b8017d8536363b377aa5e
Gaël Bonithon <gael@xfce.org>
2024-12-26 18:00:00 +0100

    XfwWorkspaceManagerWayland: Fix typo

--- libxfce4windowing/xfw-workspace-manager-wayland.c
+++ libxfce4windowing/xfw-workspace-manager-wayland.c
@@ -225,7 +225,7 @@ manager_workspace(void *data, struct ext_workspace_manager_v1 *manager, struct e
     _xfw_workspace_wayland_set_number(workspace, g_list_length(wmanager->priv->workspaces));
     wmanager->priv->workspaces = g_list_append(wmanager->priv->workspaces, workspace);
     g_signal_connect(workspace, "destroyed", G_CALLBACK(workspace_destroyed), manager);
-    g_signal_emit_by_name(manager, "workspace-created", workspace);
+    g_signal_emit_by_name(wmanager, "workspace-created", workspace);
 }
 
 static void
