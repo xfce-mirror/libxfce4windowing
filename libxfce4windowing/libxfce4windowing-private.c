cb52e854dfe620a6de91d339bacfa1b6fed07c83
Gaël Bonithon <gael@xfce.org>
2025-02-25 05:41:03 +0100

    Try to match the app id in lower case to find the .desktop file
    This should improve detection of the correct .desktop file in some
    cases, typically on X11 where the class group id is often capitalized,
    but perhaps also on Wayland.
    
    Related: https://gitlab.xfce.org/xfce/libxfce4windowing/-/issues/31#note_102421

--- libxfce4windowing/libxfce4windowing-private.c
+++ libxfce4windowing/libxfce4windowing-private.c
@@ -119,14 +119,24 @@ _xfw_g_desktop_app_info_get(const gchar *app_id) {
     app_info = g_desktop_app_info_new(desktop_id);
     g_free(desktop_id);
     if (app_info == NULL) {
-        gchar ***desktop_ids = g_desktop_app_info_search(app_id);
-        if (desktop_ids[0] != NULL) {
-            app_info = g_desktop_app_info_new(desktop_ids[0][0]);
+        gchar *lower = g_ascii_strdown(app_id, -1);
+        if (g_strcmp0(lower, app_id) != 0) {
+            desktop_id = g_strdup_printf("%s.desktop", lower);
+            app_info = g_desktop_app_info_new(desktop_id);
+            g_free(desktop_id);
         }
-        for (gchar ***p = desktop_ids; *p != NULL; p++) {
-            g_strfreev(*p);
+        g_free(lower);
+
+        if (app_info == NULL) {
+            gchar ***desktop_ids = g_desktop_app_info_search(app_id);
+            if (desktop_ids[0] != NULL) {
+                app_info = g_desktop_app_info_new(desktop_ids[0][0]);
+            }
+            for (gchar ***p = desktop_ids; *p != NULL; p++) {
+                g_strfreev(*p);
+            }
+            g_free(desktop_ids);
         }
-        g_free(desktop_ids);
     }
 
     return app_info;
