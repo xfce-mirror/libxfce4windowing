xfce-mirror/libxfce4windowing

commit 930c32a24da39bc461cc51422a06609512a45b68

XfwWorkspaceWayland: Ensure active workspace is initialized

libxfce4windowing/xfw-workspace-wayland.c
@@ -494,6 +494,11 @@ _xfw_workspace_wayland_set_workspace_group(XfwWorkspaceWayland *workspace, XfwWo
     if (group != workspace->priv->group) {
         XfwWorkspaceGroup *previous_group = workspace->priv->group;
         workspace->priv->group = group;
+        // ensure active workspace is initialized, since workspace state can be sent before
+        // workspace enters the group
+        if ((workspace->priv->state & XFW_WORKSPACE_STATE_ACTIVE) != 0) {
+            _xfw_workspace_group_wayland_set_active_workspace(XFW_WORKSPACE_GROUP_WAYLAND(group), XFW_WORKSPACE(workspace));
+        }
         g_signal_emit_by_name(workspace, "group-changed", previous_group);
     }
 }

commit bc96b103bc98798d6bd7012c4b68c310359d580e

XfwWorkspaceWayland: Add missing sanity check
Closes: #53
Fixes: 930c32a24da39bc461cc51422a06609512a45b68

libxfce4windowing/xfw-workspace-wayland.c
@@ -495,7 +495,7 @@ _xfw_workspace_wayland_set_workspace_group(XfwWorkspaceWayland *workspace, XfwWo
         workspace->priv->group = group;
         // ensure active workspace is initialized, since workspace state can be sent before
         // workspace enters the group
-        if ((workspace->priv->state & XFW_WORKSPACE_STATE_ACTIVE) != 0) {
+        if (group != NULL && (workspace->priv->state & XFW_WORKSPACE_STATE_ACTIVE) != 0) {
             _xfw_workspace_group_wayland_set_active_workspace(XFW_WORKSPACE_GROUP_WAYLAND(group), XFW_WORKSPACE(workspace));
         }
         g_signal_emit_by_name(workspace, "group-changed", previous_group);